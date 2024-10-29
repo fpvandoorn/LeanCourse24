9308d319d241ab2f6cb7dc7263f28ce20355b542
Floris van Doorn <fpvdoorn@gmail.com>
2024-10-29 15:29:24 +0100

    lecture and assignment

---
+++ LeanCourse/Assignments/Assignment4.lean
@@ -0,0 +1,156 @@
+import LeanCourse.Common
+import Mathlib.Data.Complex.Exponential
+noncomputable section
+open Real Function Set Nat
+open Classical
+
+
+/-
+
+* From Mathematics in Lean https://leanprover-community.github.io/mathematics_in_lean
+  Read chapter 4, sections 2, 3.
+
+* Do the exercises corresponding to these sections in the `LeanCourse/MIL` folder.
+  There are solutions in the solution folder in case you get stuck.
+
+* Hand in the solutions to the exercises below. Deadline: 5.11.
+
+* Make sure the file you hand-in compiles without error.
+  Use `sorry` if you get stuck on an exercise.
+-/
+
+
+/-! # Exercises to practice. -/
+
+/- Do the exercises about sets from last exercise set, if you haven't done them because
+we didn't cover the material in class yet. -/
+
+
+variable {α β γ ι : Type*} (f : α → β) (x : α) (s : Set α)
+
+/- Prove this equivalence for sets. -/
+example : s = univ ↔ ∀ x : α, x ∈ s := by {
+  sorry
+  }
+
+
+/- Prove the law of excluded middle without using `by_cases`/`tauto` or lemmas from the library.
+You will need to use `by_contra` in the proof. -/
+lemma exercise3_1 (p : Prop) : p ∨ ¬ p := by {
+  sorry
+  }
+
+/- `α × β` is the cartesian product of the types `α` and `β`.
+Elements of the cartesian product are denoted `(a, b)`, and the projections are `.1` and `.2`.
+As an example, here are two ways to state when two elements of the cartesian product are equal.
+You can use the `ext` tactic to show that two pairs are equal.
+`simp` can simplify `(a, b).1` to `a`.
+This is true by definition, so calling `assumption` below also works directly. -/
+
+example (a a' : α) (b b' : β) : (a, b) = (a', b') ↔ a = a' ∧ b = b' := Prod.ext_iff
+example (x y : α × β) : x = y ↔ x.1 = y.1 ∧ x.2 = y.2 := Prod.ext_iff
+example (a a' : α) (b b' : β) (ha : a = a') (hb : b = b') : (a, b) = (a', b') := by
+  ext
+  · simp
+    assumption
+  · assumption
+
+/- To practice, show the equality of the following pair. What is the type of these pairs? -/
+example (x y : ℝ) : (123 + 32 * 3, (x + y) ^ 2) = (219, x ^ 2 + 2 * x * y + y ^ 2) := by {
+  sorry
+  }
+
+/- `A ≃ B` means that there is a bijection from `A` to `B`.
+So in this exercise you are asked to prove that there is a bijective correspondence between
+  functions from `ℤ × ℕ` to `ℤ × ℤ`
+and
+  functions from `ℕ` to `ℕ`
+This is an exercise about finding lemmas from the library.
+Your proof is supposed to only combine results from the library,
+you are not supposed to define the bijection yourself.
+If you want, you can use `calc` blocks with `≃`. -/
+example : (ℤ × ℕ → ℤ × ℤ) ≃ (ℕ → ℕ) := by {
+  sorry
+  }
+
+/- Prove a version of the axiom of choice Lean's `Classical.choose`. -/
+example (C : ι → Set α) (hC : ∀ i, ∃ x, x ∈ C i) : ∃ f : ι → α, ∀ i, f i ∈ C i := by {
+  sorry
+  }
+
+
+/-! # Exercises to hand-in. -/
+
+
+/- ## Converging sequences
+
+Prove two more lemmas about converging sequences. -/
+
+/-- From the lecture, the sequence `u` of real numbers converges to `l`. -/
+def SequentialLimit (u : ℕ → ℝ) (l : ℝ) : Prop :=
+  ∀ ε > 0, ∃ N, ∀ n ≥ N, |u n - l| < ε
+
+/- Let's prove that reindexing a convergent sequence
+by a reindexing function that tends to infinity
+produces a sequence that converges to the same value. -/
+lemma sequentialLimit_reindex {s : ℕ → ℝ} {r : ℕ → ℕ} {a : ℝ}
+    (hs : SequentialLimit s a) (hr : ∀ m : ℕ, ∃ N : ℕ, ∀ n ≥ N, r n ≥ m) :
+    SequentialLimit (s ∘ r) a := by {
+  sorry
+  }
+
+
+/- Let's prove the squeeze theorem for sequences.
+You will want to use the lemma in the library that states
+`|a| < b ↔ -b < a ∧ a < b`. -/
+lemma sequentialLimit_squeeze {s₁ s₂ s₃ : ℕ → ℝ} {a : ℝ}
+    (hs₁ : SequentialLimit s₁ a) (hs₃ : SequentialLimit s₃ a)
+    (hs₁s₂ : ∀ n, s₁ n ≤ s₂ n) (hs₂s₃ : ∀ n, s₂ n ≤ s₃ n) :
+    SequentialLimit s₂ a := by {
+  sorry
+  }
+
+/- ## Sets -/
+
+/- Prove this without using lemmas from Mathlib. -/
+lemma image_and_intersection {α β : Type*} (f : α → β) (s : Set α) (t : Set β) :
+    f '' s ∩ t = f '' (s ∩ f ⁻¹' t) := by {
+  sorry
+  }
+
+/- Prove this by finding relevant lemmas in Mathlib. -/
+lemma preimage_square :
+    (fun x : ℝ ↦ x ^ 2) ⁻¹' {y | y ≥ 16} = { x : ℝ | x ≤ -4 } ∪ { x : ℝ | x ≥ 4 } := by {
+  sorry
+  }
+
+
+/- `InjOn` states that a function is injective when restricted to `s`.
+`LeftInvOn g f s` states that `g` is a left-inverse of `f` when restricted to `s`.
+Now prove the following example, mimicking the proof from the lecture.
+If you want, you can define `g` separately first.
+-/
+lemma inverse_on_a_set [Inhabited α] (hf : InjOn f s) : ∃ g : β → α, LeftInvOn g f s := by {
+  sorry
+  }
+
+
+/- Let's prove that if `f : α → γ` and `g : β → γ` are injective function whose
+ranges partition `γ`, then `Set α × Set β` is in bijective correspondence to `Set γ`.
+
+To help you along the way, some potentially useful ways to reformulate the hypothesis are given. -/
+
+-- This lemma might be useful.
+#check Injective.eq_iff
+
+lemma set_bijection_of_partition {f : α → γ} {g : β → γ} (hf : Injective f) (hg : Injective g)
+    (h1 : range f ∩ range g = ∅) (h2 : range f ∪ range g = univ) :
+    ∃ (L : Set α × Set β → Set γ) (R : Set γ → Set α × Set β), L ∘ R = id ∧ R ∘ L = id := by {
+  -- h1' and h1'' might be useful later as arguments of `simp` to simplify your goal.
+  have h1' : ∀ x y, f x ≠ g y := by sorry
+  have h1'' : ∀ y x, g y ≠ f x := by sorry
+  have h2' : ∀ x, x ∈ range f ∪ range g := by sorry
+  let L : Set α × Set β → Set γ := sorry
+  let R : Set γ → Set α × Set β := sorry
+  sorry
+  }
---
+++ LeanCourse/Lectures/Lecture4Before.lean
@@ -0,0 +1,200 @@
+import LeanCourse.Common
+open Real Function Set
+noncomputable section
+
+
+
+
+
+/-
+# Practical remarks
+* Assignment 4 due on 5.11. Upload it to eCampus.
+-/
+
+/- # Last Week -/
+
+/-
+How to work with `∃, ∨, ¬`:
+* Prove `∃ x, P x` using `use` and use a hypothesis using `obtain ⟨x₀, hx₀⟩ := h`
+* Prove `P ∨ Q` using `left`/`right` and use a hypothesis using `obtain hP|hQ := h`
+* Negation works like implication
+* Tactics to reason with negations and `False`:
+  `contradiction` / `exfalso`
+* Tactics for classical reasoning:
+  `by_contra h` / `by_cases h : p` / `push_neg`
+
+* I added a *tactic cheat sheet* to this repository.
+
+
+* `Set α` is the type of sets with elements from `α`
+* Prove two sets equal by using the `ext` tactic.
+* We can use the notation `{x : α | P x}` for the set of elements satisfying `P`.
+-/
+
+variable {ι α β : Type*} {s t : Set α}
+
+example : s ∩ t = {x | x ∈ s ∧ x ∈ t} := by rfl
+example : s ∪ t = {x | x ∈ s ∨ x ∈ t} := by rfl
+example : s \ t = {x | x ∈ s ∧ x ∉ t} := by rfl
+example : sᶜ = {x : α | x ∉ s} := by rfl
+example : (∅ : Set α) = {x | False} := by rfl
+example : (univ : Set α) = {x | True} := by rfl
+
+/-
+# Today: Sets (continued) and Functions
+-/
+
+/-
+## Other operations on sets
+-/
+
+/- We can take power sets. -/
+example (s : Set α) : powerset s = {t | t ⊆ s} := by rfl -- \powerset
+
+-- What is the type of `powerset s`?
+-- #check powerset s
+
+
+/- We can take unions and intersections of families of sets in three different ways:
+* Given a family of sets `C : ι → Set α`
+  we can take the union and intersection of `C i`
+  as `i` ranges over all elements of `ι`.
+-/
+example (C : ι → Set α) :
+    ⋃ i : ι, C i = {x : α | ∃ i : ι, x ∈ C i} := by ext; simp
+
+example (C : ι → Set α) :
+    ⋂ i : ι, C i = {x : α | ∀ i : ι, x ∈ C i} := by ext; simp
+
+/-
+* Given a family of sets `C : ι → Set α` and `s : Set ι`
+  we can take the union and intersection of `C i`
+  as `i` ranges over all elements in `s`.
+-/
+example (s : Set ι) (C : ι → Set α) :
+    ⋃ i ∈ s, C i = {x : α | ∃ i ∈ s, x ∈ C i} := by ext; simp
+
+example (s : Set ι) (C : ι → Set α) :
+    ⋂ i ∈ s, C i = {x : α | ∀ i ∈ s, x ∈ C i} := by ext; simp
+
+/-
+* Given a collection of sets `C : Set (Set α)`
+  we can take the union and intersection of `c`
+  for all `c ∈ C`
+-/
+
+example (𝓒 : Set (Set α)) :
+    ⋃₀ 𝓒 = {x : α | ∃ s ∈ 𝓒, x ∈ s} := by ext; simp
+
+example (𝓒 : Set (Set α)) :
+    ⋂₀ 𝓒 = {x : α | ∀ s ∈ 𝓒, x ∈ s} := by ext; simp
+
+
+example (C : ι → Set α) (s : Set α) :
+    s ∩ (⋃ i, C i) = ⋃ i, (C i ∩ s) := by {
+  sorry
+  }
+
+/- We can take images and preimages of sets.
+
+`f ⁻¹' s` is the preimage of `s` under `f`.
+`f '' s` is the image of `s` under `f`.
+
+On paper, you would write `f(A)` or `f[A]` for the image
+and `f⁻¹(B)` or `f⁻¹[B]` for the preimage.
+This notation is somewhat ambiguous, so we use different notation in Lean.
+
+-/
+
+example (f : α → β) (s : Set β) : f ⁻¹' s = { x : α | f x ∈ s } := by rfl
+
+example (f : α → β) (s : Set α) : f '' s = { y : β | ∃ x ∈ s, f x = y } := by rfl
+
+
+example {s : Set α} {t : Set β} {f : α → β} : f '' s ⊆ t ↔ s ⊆ f ⁻¹' t := by {
+  sorry
+  }
+
+
+/- We can do pointwise operations on sets (e.g. the Minkowski sum). -/
+
+open Pointwise
+
+example (s t : Set ℝ) :
+    s + t = {x : ℝ | ∃ a ∈ s, ∃ b ∈ t, a + b = x } := by rfl
+example (s t : Set ℝ) : -s = {x : ℝ | -x ∈ s } := by rfl
+
+example : ({1, 3, 5} : Set ℝ) + {0, 10} = {1, 3, 5, 11, 13, 15} := by {
+  sorry
+  }
+
+
+/-
+## Inverse of a function.
+
+Suppose we have a function `f : α → β`.
+Can we find a inverse `g : β → α` of `f`?
+Not without assuming that `f` is bijective...
+
+But suppose we try, suppose that `∃ x, f x = y`, and we want to define `g y`.
+It must be one of the `x` such that `f x = y`.
+We can choose one such `x` using *the axiom of choice*.
+-/
+
+section Inverse
+
+variable (f : α → β)
+
+#check Classical.choose
+#check Classical.choose_spec
+
+/- This doesn't look like the axiom of choice,
+since we're only choosing 1 element.
+However, this is a *function* that takes a proof
+of an existential and produces an element in `α` from it.
+This is called *global choice* and it is a bit stronger
+than the usual axiom of choice.
+As an exercise you will prove the usual axiom of choice from it.
+-/
+
+/- When working with choice,
+opening the namespace `Classical` is useful.
+If Lean complains that is cannot synthesize `Decidable` or `DecidableEq`
+then you should `open Classical`. -/
+open Classical
+
+def conditionalInverse (y : β)
+  (h : ∃ x : α, f x = y) : α :=
+  sorry
+
+lemma invFun_spec (y : β) (h : ∃ x, f x = y) :
+    f (conditionalInverse f y h) = y :=
+  sorry
+
+/- We can now define the function by cases
+on whether it lies in the range of `f` or not. -/
+
+variable [Inhabited α]
+def inverse (f : α → β) (y : β) : α :=
+  sorry
+
+/- We can now prove that `inverse f` is a right-inverse if `f` is surjective
+and a left-inverse if `f` is injective.
+We use the `ext` tactic to show that two functions are equal. -/
+lemma rightInv_of_surjective (hf : Surjective f) :
+    f ∘ inverse f = id := by {
+  sorry
+  }
+
+lemma leftInv_of_surjective (hf : Injective f) :
+    inverse f ∘ f = id := by {
+  sorry
+  }
+
+/- We can package this together in one statement. -/
+lemma inv_of_bijective (hf : Bijective f) :
+    ∃ g : β → α, f ∘ g = id ∧ g ∘ f = id := by {
+  sorry
+  }
+
+end Inverse
